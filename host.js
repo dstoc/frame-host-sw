d9eb0a0df9deec48e3a610aae66229bbf68efa1f
Douglas Stockwell <dstockwell@chromium.org>
2021-12-06 22:50:58 +1100

    Fixes to service worker JS, cleanup notification

--- host.js
+++ host.js
@@ -1,11 +1,15 @@
 const sw = navigator.serviceWorker.register('sw.js');
+const framed = !!window.parent;
 
 async function enableDelegation(origin) {
-  const notification = new Notification(`Click to allow delegationfrom ${origin}`);
+  const notification = new Notification(`Click to allow delegation`, {
+    body: `From ${origin}`,
+  });
   await new Promise(resolve => {
     notification.addEventListener('click', resolve, {once: true});
   });
   notification.close();
+  document.body.textContent = 'Ready.';
 
   const requests = {};
   navigator.serviceWorker.addEventListener('message', async e => {
@@ -21,7 +25,7 @@ if (Notification.permission == 'granted') {
   });
 } else if (Notification.permission == 'default') {
   document.body.textContent = 'Click to enable.';
-  addEventListener('click', async e => {
+  addEventListener('click', e => {
     document.textContent = 'Waiting for notification permission.';
     await Notification.requestPermission();
     location.reload();
